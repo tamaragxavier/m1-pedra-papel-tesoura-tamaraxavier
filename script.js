994367a1b6e886ac67fb86e5c603a9e7ff35e5e6
Tamara Xavier <tamaragiliski96@gmail.com>
2024-08-23 12:33:43 -0300

    desenvolvimento m1-pedra-papel-tesoura-tamaraxavier

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-/* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+    const pedra = "Pedra";
+    const papel = "Papel";
+    const tesoura = "Tesoura";
+  
+    if (player1 == pedra && player2 == pedra) {
+      return "Empate!";
+    } else if (player1 == pedra && player2 == papel) {
+      return "Jogador 2 venceu!";
+    } else if (player1 == pedra && player2 == tesoura) {
+      return "Jogador 1 venceu!";
+    } else if (player1 == papel && player2 == papel) {
+      return "Empate!";
+    } else if (player1 == papel && player2 == tesoura) {
+      return "Jogador 2 venceu!";
+    } else if (player1 == papel && player2 == pedra) {
+      return "Jogador 1 venceu!";
+    } else if (player1 == tesoura && player2 == tesoura) {
+      return "Empate!";
+    } else if (player1 == tesoura && player2 == pedra) {
+      return "Jogador 2 venceu!";
+    } else if (player1 == tesoura && player2 == papel) {
+      return "Jogador 1 venceu!";
+    }
+  }
+  
+  const resultado = playRockPaperScissor("Pedra", "Tesoura");
+  console.log(resultado);
+  
